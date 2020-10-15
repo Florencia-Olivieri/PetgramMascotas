4bb04a572a1ac02ee44ad44b0132043305cc7c9e
Florencia Olivieri <55068159+Florencia-Olivieri@users.noreply.github.com>
2020-10-15 11:24:56 -0300

    Add files via upload

---
+++ app/src/main/java/com/florenciaolivieri/petgrammascotas/PerfilFragment.java
@@ -0,0 +1,26 @@
+package com.florenciaolivieri.petgrammascotas;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class PerfilFragment extends Fragment {
+
+    public PerfilFragment(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        //el layout fragment_recyclerview está contenido en el View v
+        return inflater.inflate(R.layout.fragment_perfil, container, false);
+
+    }
+}
