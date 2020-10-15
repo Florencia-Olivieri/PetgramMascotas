6f02b77c278334a2ca2cfc10b07cc08e921f5e31
Florencia Olivieri <55068159+Florencia-Olivieri@users.noreply.github.com>
2020-10-15 16:50:51 -0300

    Add files via upload

---
+++ app/src/main/java/com/florenciaolivieri/petgrammascotas/adapter/MascotaAdaptador.java
@@ -0,0 +1,77 @@
+package com.florenciaolivieri.petgrammascotas.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.florenciaolivieri.petgrammascotas.pojo.Mascota;
+import com.florenciaolivieri.petgrammascotas.R;
+
+import java.util.ArrayList;
+
+public class MascotaAdaptador extends RecyclerView.Adapter<MascotaAdaptador.MascotaViewHolder> {
+
+
+    ArrayList<Mascota> mascotas;
+
+    /**
+     *método constructor de MascotaAdaptador
+     * construye la lista de mascotas
+     */
+
+    public MascotaAdaptador(ArrayList<Mascota> mascotas) {
+
+        this.mascotas = mascotas;
+    };
+
+
+
+    @NonNull
+    @Override
+    public MascotaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //inflar el layout y lo pasará al viewholder para que obtenga los views
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_mascota ,parent, false);
+        return new MascotaViewHolder(v);
+    }
+    @Override
+    public void onBindViewHolder(@NonNull MascotaViewHolder mascotaViewHolder, int position) {
+        //asocia cada elemento de la lista con cada view
+        Mascota mascota = mascotas.get(position);
+        mascotaViewHolder.imgMascota.setImageResource(mascota.getFoto());
+        mascotaViewHolder.tvNombreMascotaCV.setText(mascota.getNombre());
+        mascotaViewHolder.tvNLikesCV.setText(mascota.getNumeroLikes());
+
+
+    }
+
+    @Override
+    public int getItemCount() { //cantidad de elementos que contiene mi lista
+        return mascotas.size();
+    }
+
+    //clase estática le da vida a los views
+    public static class MascotaViewHolder extends RecyclerView.ViewHolder{
+
+
+        private ImageView imgMascota;
+        private TextView tvNombreMascotaCV;
+        private TextView tvNLikesCV;
+
+
+        public MascotaViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imgMascota        = (ImageView) itemView.findViewById(R.id.imgMascota);
+            tvNombreMascotaCV = (TextView) itemView.findViewById(R.id.tvNombreMascotaCV);
+            tvNLikesCV        = (TextView) itemView.findViewById(R.id.tvNLikesCV);
+
+        }
+    }
+}
+
