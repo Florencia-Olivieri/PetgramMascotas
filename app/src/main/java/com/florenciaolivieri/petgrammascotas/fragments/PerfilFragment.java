9b978407382c4fabedc2ee94a54416d7d1b92081
Florencia Olivieri <55068159+Florencia-Olivieri@users.noreply.github.com>
2020-10-17 17:44:51 -0300

    Add files via upload
    Cambios en archivos

--- app/src/main/java/com/florenciaolivieri/petgrammascotas/fragments/PerfilFragment.java
+++ app/src/main/java/com/florenciaolivieri/petgrammascotas/fragments/PerfilFragment.java
@@ -8,21 +8,57 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.florenciaolivieri.petgrammascotas.R;
+import com.florenciaolivieri.petgrammascotas.adapter.MascotaAdaptador;
+import com.florenciaolivieri.petgrammascotas.pojo.Mascota;
+
+import java.util.ArrayList;
 
 public class PerfilFragment extends Fragment {
 
-    public PerfilFragment(){
-
-    }
+    ArrayList<Mascota> mascotas;
+    private RecyclerView listaMascotas;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //return super.onCreateView(inflater, container, savedInstanceState);
         //el layout fragment_recyclerview está contenido en el View v
-        return inflater.inflate(R.layout.fragment_perfil, container, false);
+        View v = inflater.inflate(R.layout.fragment_perfil, container, false);
+
+        //manipulo el RecyclerView de Mascotas
+        listaMascotas = (RecyclerView) v.findViewById(R.id.rvMascotaFoto);
+
+        //defino cómo mostrar el RecyclerView  en una lista (LinearLayout)
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        //pongo orientación vertical para mostrar las tarjetas
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+
+        //Indico que el RecyclerView se comporte como un LinearLayout
+        listaMascotas.setLayoutManager(llm);
+
+        // Inicializo lista de Mascotas
+        inicializarListaMascotas();
+        inicializarAdaptador();
+
+        return v;
+
+    }
+    public void inicializarAdaptador() {
+        //instancio MascotaAdaptador que recibe la lista de mascotas inicializasa
+        MascotaAdaptador adaptador = new MascotaAdaptador(mascotas);
+        listaMascotas.setAdapter(adaptador);
+    }
+
+    public void inicializarListaMascotas(){
+        mascotas = new ArrayList<Mascota>();
+        mascotas.add(new Mascota(R.drawable.fotoperro_jruss,"", "7"));
+        mascotas.add(new Mascota(R.drawable.fotoperro_jruss,"", "4"));
+        mascotas.add(new Mascota(R.drawable.fotoperro_jruss,"", "8"));
+        mascotas.add(new Mascota(R.drawable.fotoperro_jruss,"", "1"));
 
     }
 }
